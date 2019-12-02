1fb5efc4c5718cfbcb1cddedd207b1d641ba6093
Marcel-Brouette <Marcel-Brouette@users.noreply.github.com>
2019-12-02 14:46:11 +0100

    Update cheat.sh

--- cheat.sh
+++ cheat.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cheats=/home/dsoria/Arsenal/Productivite/OCNSP/cheatsheets/
+cheats=~/Arsenal/Productivite/OCNSP/cheatsheets/
 if [ $1 == '-l' ]; then ls $cheats; elif [ $1 == '-u' ]; then cd $cheats/..; ./update.sh; cd -; else cat $cheats$1; fi
